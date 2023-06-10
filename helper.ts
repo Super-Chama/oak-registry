364a020b93094619d9fc42e48ff05f98708694d6
Chamara Abesinghe <chamara@orangehrmlive.com>
2023-06-10 22:29:45 +0530

    v2

--- helper.ts
+++ helper.ts
@@ -1,19 +1,49 @@
-
-import { Untar } from "https://deno.land/std@0.191.0/archive/mod.ts";
+import {
+  copy,
+  readerFromStreamReader,
+} from "https://deno.land/std@0.191.0/streams/mod.ts";
+import { Buffer } from "https://deno.land/std@0.191.0/io/mod.ts";
+import { Untar, Tar } from "https://deno.land/std@0.191.0/archive/mod.ts";
+import { ensureFile, ensureDir } from "https://deno.land/std@0.191.0/fs/mod.ts";
 
 export default async function downloadTarbal(
   url: string,
-  fileName: string
-): Promise<void> {
+  folder?: string
+): Promise<any> {
   try {
     const response = await fetch(url);
-    if (!response.ok) throw new Error(`Response not OK (${response.status})`);
-    const file = await Deno.open(`./tmp/${fileName}`, {
-      append: true,
-      create: true,
-    });
-    await response.body?.pipeTo(file.writable);
+    if (!response.ok || !response.body)
+      throw new Error(`Response not OK (${response.status})`);
+
+    const streamReader = response.body
+      .pipeThrough(new DecompressionStream("gzip"))
+      .getReader();
+    const reader = readerFromStreamReader(streamReader);
+    // const tar = new Tar();
+    const untar = new Untar(reader);
+
+    for await (const entry of untar) {
+      if (entry.type === "directory") continue;
+      entry.fileName = entry.fileName.replace(/^[^\/]*./, ""); // strip the hashname
+      const fileName = entry.fileName.match(/^[^\/]*/);
+      if (fileName && fileName.length > 0 && fileName[0] === folder) {
+        console.log(entry.fileName);
+      } else {
+        await entry.discard();
+      }
+    }
   } catch (error) {
     console.log(error);
   }
 }
+
+//   console.log(entry); // metadata
+//   /*
+//       fileName: "archive/deno.txt",
+//       fileMode: 33204,
+//       mtime: 1591657305,
+//       uid: 0,
+//       gid: 0,
+//       size: 24400,
+//       type: 'file'
+//     */
